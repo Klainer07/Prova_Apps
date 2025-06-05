63b236aa387c882643051523891722dec41b2bfe
Klainer07 <kleincruz2@gmail.com>
2025-06-05 19:53:55 -0300

    Front Eslint+Prettier

--- bucketlist/eslint.config.js
+++ bucketlist/eslint.config.js
@@ -1,18 +1,27 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import prettierPlugin from 'eslint-plugin-prettier';
 import { defineConfig } from 'eslint/config';
 export default defineConfig([
+  js.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.node,
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
+      // Suas regras personalizadas
       'prefer-const': 'error',
       'no-console': 'warn',
       'consistent-return': 'warn',
       'prefer-template': 'warn',
+      // Regras do Prettier via ESLint
+      'prettier/prettier': 'error',
     },
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
 ]);
